cd3be10972b95c51b4cc838faad4ce8611f5aaf2
mdquilindo <mdquilindo@unicauca.edu.co>
2019-11-04 14:29:33 -0500

    page 404

--- src/components/pages/pageNotFound.component.jsx
+++ src/components/pages/pageNotFound.component.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "./pageNotFound.styles.css"
 
 const PageNotFound = () => {
   return (
-    <>
-      <p>Esta pagina no existe</p>
-    </>
+    <div className="h-100 container-fluid d-flex justify-content-center align-items-center">
+      <div className="row">
+        <div className="col-12"><div className="page404-custom d-block col-12"></div></div>
+        <div className="col-12 text-center">
+          <h1 className="col-12">ERROR 404</h1> <br/>
+          <h4>La página solicitada no existe.</h4>
+        </div>
+      </div>
+    </div>
   );
 };
 
