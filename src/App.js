88675c6cf98b79a7f43a68cad72485100302cf4b
georjaos <jlmosquera@unicauca.edu.co>
2019-12-05 17:56:07 -0500

    Recuperar Contraseña pull2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Recovery from "./components/auth/recovery/recovery.component";
 import SignIn from "./components/auth/signIn/signIn.component";
 import AdminDashboard from "./components/dashboard/administrator/admin.component";
 import UserDashboard from "./components/dashboard/user/user.component";
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={SignIn} />
           <Route exact path="/admin" component={AdminDashboard} />
           <Route exact path="/user" component={UserDashboard} />
+          <Route exact path="/user/recovery/:tk" component={Recovery} />
           <Route component={PageNotFound} />
         </Switch>
       </Router>
