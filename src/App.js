748402cad43c7b3749b9a3ff030d56692fd99246
kabutoblanco <mdquilindo@unicauca.edu.co>
2019-12-14 09:39:19 -0500

    solved parcial load blank page router

--- src/App.js
+++ src/App.js
@@ -12,21 +12,24 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Switch>
-          <Route exact path='/' component={SignIn} />
-          <Route exact path='/admin/users' component={AdminDashboard} />
-          <Route exact path='/admin/studies' component={AdminDashboard} />
-          <Route exact path='/admin/users/:user' component={AdminDashboard} />
-          <Route exact path='/admin/studies/:study' component={AdminDashboard} />
-          <Route exact path='/user' component={UserDashboard} />
-          <Route exact path='/user/forget/:em' component={SignIn} />
-          <Route exact path='/user/recovery/:tk' component={Recovery} />          
-          <Route component={PageNotFound} />
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <p>Si imprimo</p>
+        <div className='App'>
+          <Switch>
+            <Route exact path='/' component={SignIn} />
+            <Route exact path='/admin/users' component={AdminDashboard} />
+            <Route exact path='/admin/studies' component={AdminDashboard} />
+            <Route path='/admin/users/:user' component={AdminDashboard} />
+            <Route path='/admin/studies/:study' component={AdminDashboard} />
+            <Route path='/user' component={UserDashboard} />
+            <Route path='/user/forget/:em' component={SignIn} />
+            <Route path='/user/recovery/:tk' component={Recovery} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/components/dashboard/administrator/admin.component.jsx
+++ src/components/dashboard/administrator/admin.component.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import Loader from 'react-loader-spinner';
-import { ProgressBar } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
 
 import { closeSession } from '../../utils/handleLocalStorage';
@@ -28,54 +27,69 @@ class AdminDashboard extends Component {
     super(props);
     this.state = {
       loading: true,
-      isLogged: true,
-      isNew: true
+      isAuthenticated: true
     };
   }
 
-  vertification = () => {
-    vertificationToken(this.source.token);
-    this.vertificationAuthorization();
+  vertification = reloaded => {
+    vertificationToken(this.source.token)
+      .then(() => {
+        this.vertificationAuthorization(reloaded);
+      })
+      .catch(error => {
+        const status = JSON.parse(error.request.status);
+        if (status === 400) {
+          closeSession();
+          alert('La sesion ha expirado.');
+          this.setState({ isAuthenticated: false });
+        }
+      });
   };
 
-  vertificationToken = () => {
-    vertificationToken(this.source.token).catch(error => {
-      const status = JSON.parse(error.request.status);
-      if (status === 400) {
-        closeSession();
-        alert('La sesion ha expirado.');
-        this.setState({ isLogged: false });
-      }
-    });
-  };
-
-  vertificationAuthorization = () => {
+  vertificationAuthorization = reloaded => {
     const headers = getHeader();
-    this.setState({ loading: true }, () =>
-      axios
-        .post(
-          URL + '/users/verificate/administrator/',
-          {},
-          { headers: headers },
-          { cancelToken: this.source.token }
+    reloaded
+      ? this.setState({ loading: true }, () =>
+          axios
+            .post(
+              URL + '/users/verificate/administrator/',
+              {},
+              { headers: headers },
+              { cancelToken: this.source.token }
+            )
+            .then(() => {
+              this.setState({ isAuthenticated: true }, () => {
+                this.setState({ loading: false });
+              });
+            })
+            .catch(error => {
+              const status = JSON.parse(error.request.status);
+              if (status === 401 || status === 500) {
+                closeSession();
+                this.setState({ isAuthenticated: false, loading: false });
+              }
+            })
         )
-        .then(() => {
-          this.setState({ isLogged: true }, () => {
-            this.setState({ loading: false });
+      : axios
+          .post(
+            URL + '/users/verificate/administrator/',
+            {},
+            { headers: headers },
+            { cancelToken: this.source.token }
+          )
+          .then(() => {
+            this.setState({ isAuthenticated: true });
+          })
+          .catch(error => {
+            const status = JSON.parse(error.request.status);
+            if (status === 401 || status === 500) {
+              closeSession();
+              this.setState({ isAuthenticated: false });
+            }
           });
-        })
-        .catch(error => {
-          const status = JSON.parse(error.request.status);
-          if (status === 401 || status === 500) {
-            closeSession();
-            this.setState({ isLogged: false });
-          }
-        })
-    );
   };
 
   contentAdmin = () => {
-    console.log(this.props.match);
     const path = this.props.match.path;
     if (path.endsWith('/users') || path.endsWith('/users/')) {
       return <ListUsers />;
@@ -89,7 +103,7 @@ class AdminDashboard extends Component {
   };
 
   componentDidMount() {
-    this.vertification();
+    this.vertification(true);
   }
 
   componentWillUnmount() {
@@ -97,7 +111,7 @@ class AdminDashboard extends Component {
   }
 
   render() {
-    if (!this.state.isLogged) {
+    if (!this.state.isAuthenticated) {
       return <Redirect to='/' />;
     }
     if (this.state.loading) {
@@ -116,7 +130,7 @@ class AdminDashboard extends Component {
       <section
         id='wrapper'
         className='h-100 container-fluid p-0'
-        onMouseDown={this.vertificationToken}>
+        onMouseDown={() => this.vertification(false)}>
         <NavBarLateral />
         <div id='content-wrapper' className='d-flex flex-column'>
           <div id='content'>
